99c94736379fb2b7a531f2e099bc704b17b5f8e3
Rosário Pereira Fernandes <rosariofernandes51@gmail.com>
2019-10-31 23:30:00 +0200

    refactor: replace information text with image banner

--- lib/screens/information.dart
+++ lib/screens/information.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:millenium/widgets/custom_button.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:photo_view/photo_view.dart';
 
 class Information extends StatefulWidget {
   @override
@@ -9,52 +8,11 @@ class Information extends StatefulWidget {
 
 class _InformationState extends State<Information> {
 
-  String title = "", text = "";
-
   @override
   Widget build(BuildContext context) {
-    if (title == '') {
-      return Center(
-        child: CircularProgressIndicator(
-          value: null,
-        ),
-      );
-    }
-    return Center(
-      child: SingleChildScrollView(
-        child :Card(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              ListTile(
-                title: CustomText(msg: title, color: Colors.pink,),
-              ),
-              Align(
-                alignment: Alignment.topLeft,
-                child: Padding(
-                    padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
-                    child: Text(text)
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+    return Container(
+      child: PhotoView(imageProvider: AssetImage('images/banner.png'))
     );
-  }
-
-  void getText() async {
-    DocumentSnapshot snapshot = await Firestore.instance.collection('ui')
-        .document('homepage').get();
-    setState(() {
-      title = snapshot.data['informationCards'][0]['title_pt'].replaceAll("\\n", "\n");
-      text = snapshot.data['informationCards'][0]['body_pt'].replaceAll("\\n", "\n");
-    });
-  }
-
-  @override
-  void initState() {
-    getText();
   }
 
 }
